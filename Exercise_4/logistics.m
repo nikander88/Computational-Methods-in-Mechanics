e1f94a05d733e0e99a245100157c94251e17970a
nikander88 <70641918+nikander88@users.noreply.github.com>
2021-11-09 19:19:28 +0200

    Add files via upload

--- Exercise_4/logistics.m
+++ Exercise_4/logistics.m
@@ -1,28 +1,23 @@
 clear all;close all;clc;
 f = @(u, t) 0.1*(1 - u/500)*u;
-loop = 1;
-U_0 = 50; % Initial y-value  
-k = 1;
-dt = 2^-k; % Time step size
-T = 5; % Maximum y-value
-
-[u, t] = ode_FE(f, U_0, dt, T);
-plot(t, u, 'b-');
-xlabel('t');  ylabel('N(t)');
-hold on
-
-
-dt = 2^-(k+1), T = 5;
-[u, t] = ode_FE(f, U_0, dt, T);
-plot(t, u, 'b-');
-xlabel('t');  ylabel('N(t)');
-
-%%
-clear all;close all;clc;
-f = @(u, t) 0.1*(1 - u/500)*u;
 
 loop = 1; k = 0; T = 60; U_0 = 100;
 
+dt = 2^-k;
+[u, t] = ode_FE(f, U_0, dt, T);
+plot(t, u, 'r-');
+xlabel('t');  ylabel('N(t)');
+hold on
+
+dt = 2^-(k+1);
+[u, t] = ode_FE(f, U_0, dt, T);
+plot(t, u, 'b-');
+xlabel('t');  ylabel('N(t)');
+legend('K+1', 'K')
+k = k+1;
+
+% Asks the user if the plots are good enough and if they aren't then it
+% it increases the amount of time steps
 while loop == 1
     prompt = 'Give 1 if the plots are not good enough: ';
     loop = input(prompt);
@@ -39,8 +34,8 @@ while loop == 1
     [u, t] = ode_FE(f, U_0, dt, T);
     plot(t, u, 'b-');
     xlabel('t');  ylabel('N(t)');
+    legend('K+1', 'K')
 
     k = k + 1
 
 end
-
